ea20d8745abba9fe6f5206e86f3509c184543036
Franz <franz.ruizsolis@gmail.com>
2018-04-05 14:38:39 -0500

    CONTAS Primera Carga

---
+++ CONTAS/Controllers/AsistenciaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CONTAS.Models;
+
+namespace CONTAS.Controllers
+{
+    
+    public class AsistenciaController : Controller
+    {
+
+        CONTASEntities db = new CONTASEntities();
+        // GET: /Asistencia/
+
+        public ActionResult Index()
+        {
+            var list = db.sp_personas_que_marcaron_hoy();
+            ViewBag.sp_personas_que_marcaron_hoy = list.ToList()    ;
+            return View();
+        }
+
+    }
+}
